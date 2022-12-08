cc359b7d6e9dbf8b69da3d6891ea58c137fbcd89
Ruben Del Rio <me@rdelrio.com>
2022-12-08 16:45:47 -0500

    test(shared): :white_check_mark: testing suggestion methods

--- packages/shared/test/shared.spec.ts
+++ packages/shared/test/shared.spec.ts
@@ -1,15 +1,37 @@
-import { describe, expect, expectTypeOf, test } from 'vitest'
-import { movies, noop } from "../src/index"
+import { describe, expect, expectTypeOf, test, vi } from 'vitest'
+import {
+  getMovieSuggestionIds,
+  getRandomItems,
+} from "../src/index"
 
-describe('shared', () => {
-  test('Movies', () => {
-    // expect(movies)
-    const mv = JSON.stringify(movies)
-    expect(mv).eq("{}")
+import json from '../../../data/movies.json'
+
+// One-liner PRNG
+// https://gist.github.com/blixt/f17b47c62508be59987b?permalink_comment_id=2682175#gistcomment-2682175
+const LCG=(s:number)=>()=>(s=(Math.imul(48271,s)>>>0)%2147483647)/2147483647;
+
+describe('shared', async () => {
+  test('getMovieSuggestionIds', async () => {
+    // vi.spyOn(Math, 'random').mockReturnValue(5)
+    let prng = LCG(100)
+
+    // Mock random-feeling numbers
+    Math.random = () => prng()
+    
+    expect(
+      await getMovieSuggestionIds(json.movies, 4, [36])
+    ).toEqual([1, 37, 10, 31])
   })
 
-  test('noop', () => {
-    // expect(movies)
-    expectTypeOf(noop).toBeVoid(undefined as never)
+  test('getRandomItems', () => {
+    let prng = LCG(100)
+    Math.random = () => prng()
+
+    expect(
+      getRandomItems(
+        ['foo', 'bar', false, NaN, 'baz', 1, 2, 3, 4],
+        5
+      )
+    ).toEqual(['foo', 1, 'bar', 'baz', false])
   })
 })
