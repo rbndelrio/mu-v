a3b6518cb78a5e832401574336db081c7410d05a
Ruben Del Rio <me@rdelrio.com>
2022-12-09 09:54:28 -0500

    test(shared): :white_check_mark: add picker tests

--- packages/shared/test/shared.spec.ts
+++ packages/shared/test/shared.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest';
 
 import { getMovieSuggestionIds } from "../src/index";
 import { getRandomItems } from '../src/random';
+import { pickFirst, pickItems, pickRandom, pickWeightedRandom } from '../src/weighted';
 
 import json from '../../../data/movies.json';
 
@@ -32,5 +33,35 @@ describe('shared', async () => {
         5
       )
     ).toEqual(['foo', 1, 'bar', 'baz', false])
+  })
+
+  test('picker', () => {
+    let prng = LCG(999999)
+    Math.random = () => prng()
+    Math.random() // burn first item (always a low number)
+
+    expect(pickItems(), 'default output should be empty array')
+      .toEqual([])
+
+    expect(pickItems([1, 2, 3]), 'single param input results in no change')
+      .toEqual([1, 2, 3])
+
+    expect(pickItems([1, 2, 3], pickFirst), 'pickFirst always picks first item')
+      .toEqual([1, 2, 3])
+
+    expect(pickItems([1, 2, 3], pickFirst, 1), 'limit param constrains output')
+      .toEqual([1])
+
+    expect(
+      pickItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], pickRandom, 4),
+      'pickRandom picks randomly'
+    ).toEqual([4, 7, 2, 1])
+
+    Math.random() // burn another number
+
+    expect(
+      pickItems([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], pickWeightedRandom, 4),
+      'pickWeightedRandom prefers the front quarter of items'
+    ).toEqual([1, 2, 3, 6])
   })
 })
